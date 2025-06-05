af06162520ff8ed6e8b96112f1b9a22d6c8077c0
Marcel Wiessler <marcel@gaisterhand.de>
2025-06-05 19:14:05 +0200

    Fix output

--- src/publish.js
+++ src/publish.js
@@ -300,7 +300,7 @@ export async function publish(args) {
     // is github CI?
     if (process.env.GITHUB_ACTIONS) {
         if (process.env.GITHUB_OUTPUT) {
-            logger.info(`Running in GitHub Actions environment. Setting output variables.`);
+            logger.info(`Running in GitHub Actions environment. Setting output variables to ${process.env.GITHUB_OUTPUT}`);
             appendFileSync(process.env.GITHUB_OUTPUT, `package-version=${packageJson.version}\n`);
             appendFileSync(process.env.GITHUB_OUTPUT, `package-name=${packageJson.name}\n`);
             appendFileSync(process.env.GITHUB_OUTPUT, `package-published=${needsPublish}\n`);
