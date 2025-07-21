fc512d030ea36c4e512b0d5a0a7c5bb3984661a5
Marcel Wiessler <marcel@gaisterhand.de>
2025-07-21 10:59:14 +0200

    Pass logger through compile functions + fix dir error

--- src/npmdef.js
+++ src/npmdef.js
@@ -2,16 +2,21 @@
 import { existsSync, readdirSync, readFileSync, writeFileSync } from "fs";
 
 
-export async function updateNpmdef() {
+/**
+ * Updates the npmdef files and Unity package.json based on the current package.json.
+ * @param {{logger:import("@caporal/core").Logger} | null} options
+ */
+export async function updateNpmdef(options) {
     /** @type {import("../types").PackageJson} */
     const currentPackageJson = JSON.parse(readFileSync("package.json", "utf8"));
-    updateUnityPackage(currentPackageJson);
+    updateUnityPackage(currentPackageJson, options);
 }
 
 /**
  * @param {import("../types").PackageJson} packageJson
+ * @param {{logger:import("@caporal/core").Logger} | null} options
  */
-function updateUnityPackage(packageJson) {
+function updateUnityPackage(packageJson, options) {
 
     const dir = process.cwd() + "/unity";
     if (existsSync(dir)) {
@@ -23,19 +28,22 @@ function updateUnityPackage(packageJson) {
         unityPackageJson.version = packageJson.version;
         if (packageJson.description) unityPackageJson.description = packageJson.description;
         writeFileSync(unityPackageJsonPath, JSON.stringify(unityPackageJson, null, 4));
-        console.log("Updated unity package.json");
+        options?.logger.info(`Updated unity package.json at ${unityPackageJsonPath}`);
 
 
         // Update npmdefs
         const npmdefs = readdirSync(dir).filter(f => f.endsWith(".npmdef"));
         for (const npmdef of npmdefs) {
             const fp = dir + "/" + npmdef;
-            console.log(`Update npmdef: ${fp}`);
+            options?.logger.info(`Update npmdef: ${fp}`);
             /** @type {import("../types").Npmdef} */
             const content = JSON.parse(readFileSync(fp, "utf8"));
             content.packageName = packageJson.name;
             content.packageVersion = packageJson.version;
             writeFileSync(fp, JSON.stringify(content, null, 4));
         }
+    }
+    else {
+        options?.logger.warn(`No unity directory found at ${dir}. Skipping npmdef update.`);
     }
 }
